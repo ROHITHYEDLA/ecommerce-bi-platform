46be1c4f5d7af53ff90a080883143be02c29c88e
Rohith <rohithyedla7330@gmail.com>
2026-08-02 21:56:17 +0530

    added views modee

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductSerializer
+from .services import ProductService
+from .permissions import IsAdminOrReadOnly
+
+
+class ProductListCreateView(generics.ListCreateAPIView):
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+    def perform_create(self, serializer):
+        ProductService.create_product(serializer)
+
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+    def perform_update(self, serializer):
+        ProductService.update_product(serializer)
+
+    def perform_destroy(self, instance):
+        ProductService.delete_product(instance)
